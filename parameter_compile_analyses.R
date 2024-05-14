b06a154c165b0763cecc6711f12e9280c7fb9add
Mark Miller <mark.miller1@monash.edu>
2024-05-14 14:55:41 +1000

    Speed data formatted and prepped for random effect model meta-analysis

--- parameter_compile_analyses.R
+++ parameter_compile_analyses.R
@@ -6,6 +6,9 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 library(ggrepel)
+library(meta)
+library(estmeansd)
+
 
 rm(list=ls())
 setwd("C:/Users/mmil0049/OneDrive - Monash University/projects/01 southern seabird OWF review/")
@@ -15,7 +18,7 @@ setwd("C:/Users/mmil0049/OneDrive - Monash University/projects/01 southern seabi
 #### *** Read in data and formatting *** ####
 
 #flight height sheet
-dat_FLH<-read_xlsx('C:/Users/mmil0049/Downloads/procellariiform_OWF_review_FORMATTED (3).xlsx', sheet='flight.height', skip=1) # skip top 'checking' row
+dat_FLH<-read_xlsx('C:/Users/mmil0049/Downloads/procellariiform_OWF_review_FORMATTED (9).xlsx', sheet='flight.height', skip=1) # skip top 'checking' row
 
 height_meta<-data.frame(ref=paste(dat_FLH[1,7:ncol(dat_FLH)]), str_split_fixed(dat_FLH[3,7:ncol(dat_FLH)], "@", 5))
 names(height_meta)[2:6]=c("data.type", "place", "country", "marine region", "stage")
@@ -26,7 +29,7 @@ dat_FLH<-dat_FLH[-which(is.na(dat_FLH[,1])),] # remove NA row normally.. at end
 
 
 #flight speed sheet
-dat_FSD<-read_xlsx('C:/Users/mmil0049/Downloads/procellariiform_OWF_review_FORMATTED (3).xlsx', sheet='flight.speed', skip=1) # skip top 'checking' row
+dat_FSD<-read_xlsx('C:/Users/mmil0049/Downloads/procellariiform_OWF_review_FORMATTED (9).xlsx', sheet='flight.speed', skip=1) # skip top 'checking' row
 
 speed_meta<-data.frame(ref=paste(dat_FSD[1,7:ncol(dat_FSD)]), str_split_fixed(dat_FSD[3,7:ncol(dat_FSD)], "@", 5))
 names(speed_meta)[2:6]=c("data.type", "place", "country", "marine region", "stage")
@@ -158,26 +161,26 @@ ggplot()+
 
 #### *** Flight speed review *** ####
 
-## FLIGHT HEIGHT ##
 
-# run per species
-# use random effects model making each study@stage a separate slab BUT using study as the random effect grouping level.
+#### *** prep data and cast to long format *** #### 
+
+# function 1 splitting data
+meta_split<-function(x)
+{
+  t1<-unlist(str_split(x, "@"))
+  if(length(t1)==5)
+  {
+    t2<-as.data.frame(as.list(c('Combined', t1)), col.names=(c('V1', "V2", "V3", "V4", "V5", "V6")))
+  }else{
+    t2<-do.call("rbind", (split(t1, ceiling(1:length(t1)/6))))%>%as.data.frame()
+  }
+  return(t2)
+}
+
+## FLIGHT SPEED ##
 
 fl_speed<-dat_FSD%>%dplyr::select(c(1:6, starts_with('flight')))
 names(fl_speed)<-fl_speed[1,];fl_speed<-fl_speed[-1,]
-
-
-meta_split<-function(x)
-  {
-  t1<-unlist(str_split(x, "@"))
-  if(length(t1)==5)
-    {
-    t2<-as.data.frame(as.list(c('Combined', t1)), col.names=(c('V1', "V2", "V3", "V4", "V5", "V6")))
-    }else{
-    t2<-do.call("rbind", (split(t1, ceiling(1:length(t1)/6))))%>%as.data.frame()
-    }
-  return(t2)
-  }
  
 long_speed<-NULL
 for(i in 1:nrow(fl_speed))
@@ -186,25 +189,145 @@ for(i in 1:nrow(fl_speed))
   sel_sp<-fl_speed_temp[i,which(!is.na(fl_speed_temp[i,]))]
   if(length(sel_sp)==0){next} # skip sp with no data
   temp<-do.call("rbind", apply(sel_sp, 2, FUN=meta_split))
-  temp<-data.frame(sp=fl_speed[i,]$`Common name`, study=row.names(temp), temp)
+  temp<-data.frame(sp=fl_speed[i,]$`Common name`,
+                   study=paste0(unlist(str_split(row.names(temp), "\\)"))[seq(1, ((nrow(temp)*2)-1), 2)], ")"), temp)
   long_speed<-rbind(long_speed, temp)
 }
  
 row.names(long_speed)<-NULL 
   
 
+## MAX SPEED ##
+
+max_speed<-dat_FSD%>%dplyr::select(c(1:6, starts_with('max')))
+names(max_speed)<-max_speed[1,];max_speed<-max_speed[-1,]
+
+long_max<-NULL
+for(i in 1:nrow(max_speed))
+{
+  max_speed_temp<-max_speed[,7:ncol(max_speed)]
+  sel_sp<-max_speed_temp[i,which(!is.na(max_speed_temp[i,]))]
+  if(length(sel_sp)==0){next} # skip sp with no data
+  temp<-do.call("rbind", apply(sel_sp, 2, FUN=meta_split))
+  temp<-data.frame(sp=max_speed[i,]$`Common name`,
+                   study=paste0(unlist(str_split(row.names(temp), "\\)"))[seq(1, ((nrow(temp)*2)-1), 2)], ")"), temp)
+  long_max<-rbind(long_max, temp)
+}
+
+row.names(long_max)<-NULL 
+
+long_max[long_max$V3==99,]$V3<-0 # set max with no sd (max max)to zero as few species where this is an issue
+
+
+## TRIP SPEED ##
+
+trip_speed<-dat_FSD%>%dplyr::select(c(1:6, starts_with('trip')))
+names(trip_speed)<-trip_speed[1,];trip_speed<-trip_speed[-1,]
+
+long_trip<-NULL
+for(i in 1:nrow(trip_speed))
+{
+  trip_speed_temp<-trip_speed[,7:ncol(trip_speed)]
+  sel_sp<-trip_speed_temp[i,which(!is.na(trip_speed_temp[i,]))]
+  if(length(sel_sp)==0){next} # skip sp with no data
+  temp<-do.call("rbind", apply(sel_sp, 2, FUN=meta_split))
+  temp<-data.frame(sp=trip_speed[i,]$`Common name`,
+                   study=paste0(unlist(str_split(row.names(temp), "\\)"))[seq(1, ((nrow(temp)*2)-1), 2)], ")"), temp)
+  long_trip<-rbind(long_trip, temp)
+}
+
+row.names(long_trip)<-NULL 
+
+## combine all speed data
+
+all_speed<-rbind(data.frame(varib="max", long_max),data.frame(varib="trip", long_trip),
+      data.frame(varib="speed", long_speed))
+
+#export dataset to check sample sizes for calculating mean and sd from median and min/max
+#write_xlsx(all_speed[all_speed$V3=="NA",],"analyses/median_ss_checking.xlsx")
+
+
+# Now impute missing means and SDs from median, min and max
+# function using estmeansd package, using MLN method 
+MLN_mnsd<-function(x){
+  qe1<-mln.mean.sd(min.val = as.numeric(x["V4"]), med.val = as.numeric(x["V2"]),
+                  max.val = as.numeric(x["V5"]), n = as.numeric(x["med_min_max_SS"]))
+  rbind(qe1$est.mean, qe1$est.sd)}
+
+# read in median/min/max sample size sheet 
+med_mi_ma<-read_excel("analyses/median_ss_checking.xlsx")
+
+med_mi_ma[c("V2", "V3")]<- round(t(apply(med_mi_ma, 1, FUN=MLN_mnsd)),2) # apply function and replace median and NA SD vals with calculatd vals
+
+#recombine datasets and export
+
+speed_ready<-rbind(med_mi_ma[c("varib" ,"sp","study","V1", "V2", "V3", "V6")],
+      all_speed[all_speed$V3!="NA",c("varib" ,"sp","study","V1", "V2", "V3", "V6")])
+
+names(speed_ready)[4]<-"subset"
+names(speed_ready)[5]<-"mean"
+names(speed_ready)[6]<-"sd"
+names(speed_ready)[7]<-"n"
+
+speed_ready$mean<-as.numeric(speed_ready$mean)
+speed_ready$sd<-as.numeric(speed_ready$sd)
+speed_ready$n<-as.numeric(speed_ready$n)
+
+speed_ready<-speed_ready%>%arrange("varib", "study")
+
+speed_ready<-left_join(speed_ready, speed_meta, by=c("study"="ref"), multiple='first') # join in meta data
+
+#write_xlsx(speed_ready,"analyses/speed_ready.xlsx")
+      
+#### ***  *** ####
+
+#### *** Run meta-analysis: mixed models *** #### 
+
+# run per species
+# use random effects model making each study@stage a separate slab BUT using study as the random effect grouping level.
+
+
+# trial
+
+# apply function to fill NA mean and sd columns from median, min and max
 
 
 
-
-
+for(j in unique(long_trip$sp))
+{
+  temp1<-long_trip[long_trip$sp==j,]
   
-  t1$V2<-str_replace_all(t1$V2,c(L="0.33", M="0.66", H="1"))
-  t1<-t1 %>% mutate_if(is.character,as.numeric)
-  return(weighted.mean(t1$V1, t1$V2, na.rm=T))}
+  m.mean <- metamean(n = as.numeric(V6),
+                     mean = as.numeric(V2),
+                     sd = as.numeric(V3),
+                     studlab = study,
+                     cluster=study,
+                     data = temp1,
+                     sm = "MLN", # use log transformation instead. Probably, advisable when using non-negative data
+                     fixed = FALSE,
+                     random = TRUE,
+                     method.tau = "REML",
+                     title = paste(i, "Scores"))
+  print(summary(m.mean))
+}
 
-fl_height$ave_fl_height<-apply(fl_height, 1, FUN=wt_mn)
 
-sel_sp<-fl_speed[i,]
 
+## FLIGHT SPEED ##
+
+
+
+## MAX SPEED ##
+
+
+
+## TRIP SPEED ##
+
+row.names(long_trip)<-NULL 
+
+#export dataset to check sample sizes for calculating mean and sd from median and min/max
+#write_xlsx(rbind(data.frame(varib="max", long_max[long_max$V3=="NA",]),data.frame(varib="trip", long_trip[long_trip$V3=="NA",]),
+#data.frame(varib="speed", long_speed[long_speed$V3=="NA",])),"analyses/median_ss_checking.xlsx")
+
+#### ***  *** ####
 
